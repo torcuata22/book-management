1689c9b15b3a413a5ae2b1757785df7445f25d5c
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2024-12-30 14:35:10 -0500

    debugging imports

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"net/http"
+)
+
+var (
+	CreateBook  = func(w http.ResponseWriter, r *http.Request) {}
+	GetBook     = func(w http.ResponseWriter, r *http.Request) {}
+	GetBookById = func(w http.ResponseWriter, r *http.Request) {}
+	UpdateBook  = func(w http.ResponseWriter, r *http.Request) {}
+	DeleteBook  = func(w http.ResponseWriter, r *http.Request) {}
+)
